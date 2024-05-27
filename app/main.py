7e7952e996f2ac3e7ceb37fcfaf38fb001638d06
MatiwosD <matiwos.desalegn@hst-et.com>
2024-05-27 20:07:19 +0300

    working on the app

---
+++ app/main.py
@@ -0,0 +1,35 @@
+# app/main.py
+
+from flask import Flask, request, jsonify, render_template
+import joblib
+import tensorflow as tf
+import pandas as pd
+
+app = Flask(__name__)
+
+# Load the models
+ml_model = joblib.load('data/models/model-<timestamp>.pkl')
+dl_model = tf.keras.models.load_model('data/models/lstm_model-<timestamp>.h5')
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict_ml', methods=['POST'])
+def predict_ml():
+    data = request.get_json(force=True)
+    data_df = pd.DataFrame(data)
+    prediction = ml_model.predict(data_df)
+    return jsonify(prediction.tolist())
+
+@app.route('/predict_dl', methods=['POST'])
+def predict_dl():
+    data = request.get_json(force=True)
+    data_df = pd.DataFrame(data)
+    scaled_data = scaler.transform(data_df)
+    X = scaled_data.reshape((scaled_data.shape[0], scaled_data.shape[1], 1))
+    prediction = dl_model.predict(X)
+    return jsonify(prediction.tolist())
+
+if __name__ == "__main__":
+    app.run(debug=True)
