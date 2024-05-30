11cdfe8a46756e44546175c244f9c6515e790d37
MatiwosD <matiwos.desalegn@hst-et.com>
2024-05-30 23:20:20 +0300

    Working on time series analysis

--- scripts/data_loading.py
+++ scripts/data_loading.py
@@ -5,16 +5,23 @@ logger = logging.getLogger(__name__)
 
 def load_data(train_path, test_path, store_path):
     """
-    Load the train, test, and store datasets.
+    Load the train, test, and store datasets with specified parameters.
+    
     Args:
         train_path (str): Path to the training dataset.
         test_path (str): Path to the test dataset.
         store_path (str): Path to the store dataset.
+        
     Returns:
         tuple: DataFrames for train, test, and store datasets.
     """
-    train = pd.read_csv(train_path)
-    test = pd.read_csv(test_path)
-    store = pd.read_csv(store_path)
-    logger.info("Data loaded successfully")
+    try:
+        train = pd.read_csv(train_path, parse_dates=True, low_memory=False, index_col=0)
+        test = pd.read_csv(test_path, parse_dates=True, low_memory=False, index_col=0)
+        store = pd.read_csv(store_path, low_memory=False)
+        logger.info("Data loaded successfully")
+    except Exception as e:
+        logger.error(f"Error loading data: {e}")
+        raise e
+    
     return train, test, store
